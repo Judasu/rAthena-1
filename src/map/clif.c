Judasu/rAthena-1

commit 1b79df3ea183044b26edcf879e91e237221ea45c

- Fixed a visual itemdupple bug on mail system. - Fixed Overthrusth status cannot being recalled at same skill lvl.
git-svn-id: http://rathena.googlecode.com/svn/trunk@12023 dfcd82a3-dffb-8f83-6aec-effe02521c5c

src/map/clif.c
@@ -7048,6 +7048,9 @@ int clif_message(struct block_list* bl, const char* msg)
 int clif_refresh(struct map_session_data *sd)
 {
 	nullpo_retr(-1, sd);
+
+	mail_clear(sd);
+
 	clif_changemap(sd,sd->mapindex,sd->bl.x,sd->bl.y);
 	clif_inventorylist(sd);
 	if(pc_iscarton(sd)) {
@@ -7687,6 +7690,8 @@ void clif_parse_LoadEndAck(int fd,struct map_session_data *sd)
 		sd->state.connect_new = 0;
 		return;
 	}
+
+	mail_clear(sd);
 
 	if (sd->state.rewarp)
   	{	//Rewarp player.

src/map/mail.c
@@ -20,6 +20,16 @@ time_t mail_calctimes(void)
 	return mktime(localtime(&temp));
 }
 
+void mail_clear(struct map_session_data *sd)
+{
+	sd->mail.nameid = 0;
+	sd->mail.index = 0;
+	sd->mail.amount = 0;
+	sd->mail.zeny = 0;
+
+	return;
+}
+
 int mail_removeitem(struct map_session_data *sd, short flag)
 {
 	nullpo_retr(0,sd);

src/map/mail.h
@@ -8,6 +8,7 @@
 
 time_t mail_calctimes(void);
 
+void mail_clear(struct map_session_data *sd);
 int mail_removeitem(struct map_session_data *sd, short flag);
 int mail_removezeny(struct map_session_data *sd, short flag);
 unsigned char mail_setitem(struct map_session_data *sd, int idx, int amount);

src/map/status.c
@@ -4925,7 +4925,7 @@ int status_change_start(struct block_list* bl,enum sc_type type,int rate,int val
 			case SC_ADRENALINE2:
 			case SC_WEAPONPERFECTION:
 			case SC_OVERTHRUST:
-				if (sce->val2 > val2)
+				if (sce->val1 > val1)
 					return 0;
 			break;
 			case SC_HPREGEN:

commit 0ffd04be4e209d26b16c1ecc5e823709577840e6

Corrected a TXT compilation problem caused by r12023 (mail system code is not present in TXT builds).
git-svn-id: http://rathena.googlecode.com/svn/trunk@12026 dfcd82a3-dffb-8f83-6aec-effe02521c5c

src/map/clif.c
@@ -7049,8 +7049,6 @@ int clif_refresh(struct map_session_data *sd)
 {
 	nullpo_retr(-1, sd);
 
-	mail_clear(sd);
-
 	clif_changemap(sd,sd->mapindex,sd->bl.x,sd->bl.y);
 	clif_inventorylist(sd);
 	if(pc_iscarton(sd)) {
@@ -7073,6 +7071,11 @@ int clif_refresh(struct map_session_data *sd)
 		clif_send_homdata(sd,0,0);
 	map_foreachinrange(clif_getareachar,&sd->bl,AREA_SIZE,BL_ALL,sd);
 	clif_weather_check(sd);
+
+#ifndef TXT_ONLY
+	mail_clear(sd);
+#endif
+
 	return 0;
 }
 
@@ -7691,8 +7694,6 @@ void clif_parse_LoadEndAck(int fd,struct map_session_data *sd)
 		return;
 	}
 
-	mail_clear(sd);
-
 	if (sd->state.rewarp)
   	{	//Rewarp player.
 		sd->state.rewarp = 0;
@@ -7883,6 +7884,10 @@ void clif_parse_LoadEndAck(int fd,struct map_session_data *sd)
 
 		if(sd->npc_id)
 			npc_event_dequeue(sd);
+
+#ifndef TXT_ONLY
+		mail_clear(sd);
+#endif
 	}
 
 	if(map[sd->bl.m].flag.loadevent) // Lance